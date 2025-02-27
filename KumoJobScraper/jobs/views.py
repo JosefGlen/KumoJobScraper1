8100a44396f2e773380c90cc54f9fe95aa396254
Cr1m <38981037+Cr1mson901@users.noreply.github.com>
2025-02-26 21:30:17 -0500

    Added in login requirment for saved page and job page

--- KumoJobScraper/jobs/views.py
+++ KumoJobScraper/jobs/views.py
@@ -10,10 +10,12 @@ def jobs_list(request):
     jobs = Jobs.objects.all().order_by('-date')
     return render(request, 'jobs/jobs_list.html', { 'jobs': jobs})
 
+@login_required(login_url="/users/login")
 def job_page(request, slug):
     # Gets the only job that matches the slug
     job = Jobs.objects.get(slug=slug)
     return render(request, 'jobs/job_page.html', { 'job': job})
 
+@login_required(login_url="/users/login")
 def jobs_saved(request):
     return render(request, 'jobs/jobs_saved.html')
