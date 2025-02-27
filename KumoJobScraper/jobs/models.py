f277d551895c8c5e2874d710aae9437c828b79dc
Cr1m <38981037+Cr1mson901@users.noreply.github.com>
2025-02-26 22:05:36 -0500

    Update models.py
    Accidentally pushed this to main, reverted to not cause errors

--- KumoJobScraper/jobs/models.py
+++ KumoJobScraper/jobs/models.py
@@ -6,22 +6,9 @@ class Jobs(models.Model):
     title = models.CharField(max_length=75)
     description = models.TextField()
     company = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)
     salary = models.PositiveIntegerField()
     date = models.DateField(auto_now_add=True)
     slug = models.SlugField()
 
     def __str__(self):
         return self.title
-    
-class SavedJob(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    job = models.ForeignKey(Jobs, on_delete=models.CASCADE)
-    saved_on = models.DateTimeField(auto_now_add=True)
-
-    # Makes it so that users can't save the same job multiple times
-    class Meta:
-        unique_together = ('user', 'job')
-    
-    def __str__(self):
-        return f"{self.user.username} - {self.job.title}"
