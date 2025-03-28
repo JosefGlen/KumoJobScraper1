8cd1c5293988fc55efd358acad72724889005855
Cr1m <38981037+Cr1mson901@users.noreply.github.com>
2025-03-28 15:22:44 -0400

    Updated Salary field to be compatible with the data, removed slug from model

--- KumoJobScraper/jobs/models.py
+++ KumoJobScraper/jobs/models.py
@@ -6,10 +6,9 @@ class Jobs(models.Model):
     title = models.CharField(max_length=75)
     description = models.TextField()
     company = models.CharField(max_length=50)
-    location = models.CharField(max_length=50, default="Unknown")
-    salary = models.PositiveIntegerField()
+    location = models.CharField(max_length=50, default="Location Unknown")
+    salary = models.CharField(max_length=50, default="Salary Unknown")
     date = models.DateField(auto_now_add=True)
-    slug = models.SlugField()
 
     def __str__(self):
         return self.title
